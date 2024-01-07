76f6fd41d40ce775ef57634c2cd3c42f8df1759e
LuanFabricio <luanlimaleite@hotmail.com>
2024-01-07 01:43:04 -0300

    feat: Adding a pointer to display fuel and updating inner function names

--- player.c
+++ player.c
@@ -9,19 +9,20 @@
 #include "utils.h"
 #define PLAYER_COLOR BLUE
 
-#define MAX_FUEL 100000;
+#define MAX_FUEL 50000
 
 // Inner functions declarations
-void player_display_fuel(Ship player);
-void player_move(Ship* player);
+void _player_display_fuel(Ship player);
+void _player_move(Ship* player);
 
 // Extern functions implementation
+
 void player_start(Ship *player)
 {
 	player->size = CLITERAL(Vector2){42.f, 42.f};
 
-	player->pos.x = (GAME_MAX_X - GAME_MIN_X) / 2.f;
-	player->pos.y = (GAME_MAX_Y - GAME_MIN_Y) / 2.f;
+	player->pos.x = (GAME_MAX_X + GAME_MIN_X) / 2.f;
+	player->pos.y = (GAME_MAX_Y + GAME_MIN_Y) / 2.f;
 
 	player->fuel = MAX_FUEL;
 }
@@ -39,36 +40,60 @@ void player_draw_game(const Ship player)
 
 void player_draw_ui(Ship player)
 {
-	player_display_fuel(player);
+	_player_display_fuel(player);
 }
 
 void player_update(Ship* player)
 {
-	player->fuel -= 0.5f;
-	player_move(player);
+	if (player->fuel > 0) {
+		player->fuel -= 0.5f;
+	} else {
+		player->fuel = 0;
+	}
+
+	_player_move(player);
 }
 
 // Inner functions implementation
 
-void player_display_fuel(Ship player)
+void _player_display_fuel(Ship player)
 {
+	// Debug implementation
 	const size_t BUFFER_SIZE = 255;
 	const size_t FONT_SIZE = 32;
 
 	char* fuel_text = (char*)malloc(sizeof(char) * BUFFER_SIZE);
-	snprintf(fuel_text, BUFFER_SIZE, "Fuel remaning: %.2f", player.fuel);
+	snprintf(fuel_text, BUFFER_SIZE, "Fuel remaning: %.02f", player.fuel);
 
 	int width = MeasureText(fuel_text, FONT_SIZE);
 	int pos_x = (GetScreenWidth() - width) >> 1;
 	int pos_y = FONT_SIZE;
 
-
 	DrawText(fuel_text, pos_x, pos_y, FONT_SIZE, RAYWHITE);
 
 	free(fuel_text);
+
+	// Final implementation
+	float fuel = Normalize(player.fuel, 0, MAX_FUEL) * -PI - PI;
+
+	Vector2 start = {
+		.x = (GAME_MAX_X + GAME_MIN_X) / 2.f,
+		.y = GAME_MIN_Y + 5.f,
+	};
+
+	printf("start.x = %.02f\nstart.y = %.02f\n", start.x, start.y);
+
+	Vector2 end = {
+		.x  = cosf(fuel),
+		.y  = -sinf(fuel),
+	};
+	end = Vector2Scale(end, 3.f);
+	end = Vector2Add(end, start);
+
+	DrawLineEx(project_game_to_screen(start), project_game_to_screen(end), 3.5f, RAYWHITE);
 }
 
-void player_move(Ship* player)
+void _player_move(Ship* player)
 {
 	Vector2 acceleration = Vector2Zero();
 
