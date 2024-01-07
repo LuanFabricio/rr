6a14c8746d4f624ef11803288c88a250c1fa09aa
LuanFabricio <luanlimaleite@hotmail.com>
2024-01-07 00:58:54 -0300

    feat: Adding a macro to create function pointers and updating functions

--- player.h
+++ player.h
@@ -26,14 +26,19 @@ typedef struct {
 RR_PLAYER Ship player = {0};
 RR_PLAYER void* player_shared_ptr = NULL;
 
-typedef void (*player_start_t)(Ship*);
-RR_PLAYER player_start_t player_start =  NULL;
+#define CREATE_FUNCTION_PTR(name, args, return)\
+	typedef return (*name##_t)args;\
+	RR_PLAYER name##_t name =  NULL
 
-typedef void (*player_draw_t)(Ship*);
-RR_PLAYER player_draw_t player_draw =  NULL;
+CREATE_FUNCTION_PTR(player_start, (Ship*), void);
 
-typedef void (*player_move_t)(Ship*);
-RR_PLAYER player_move_t player_move =  NULL;
+CREATE_FUNCTION_PTR(player_draw_game, (Ship), void);
+
+CREATE_FUNCTION_PTR(player_update, (Ship*), void);
+
+CREATE_FUNCTION_PTR(player_draw_ui, (Ship), void);
+
+#undef CREATE_FUNCTION_PTR
 
 typedef void (*player_display_fuel_t)(Ship);
 RR_PLAYER player_display_fuel_t player_display_fuel = NULL;
@@ -46,8 +51,9 @@ RR_PLAYER void reset_player_function()
 	player_shared_ptr = hr_reset_file(file_path, player_shared_ptr);
 
 	player_start = (player_start_t)hr_reset_function(player_shared_ptr, "player_start");
-	player_draw = (player_draw_t)hr_reset_function(player_shared_ptr, "player_draw");
-	player_move = (player_move_t)hr_reset_function(player_shared_ptr, "player_move");
+	player_draw_game = (player_draw_game_t)hr_reset_function(player_shared_ptr, "player_draw_game");
+	player_update = (player_update_t)hr_reset_function(player_shared_ptr, "player_update");
+	player_draw_ui = (player_draw_ui_t)hr_reset_function(player_shared_ptr, "player_draw_ui");
 	player_display_fuel = (player_display_fuel_t)hr_reset_function(player_shared_ptr, "player_display_fuel");
 }
 
