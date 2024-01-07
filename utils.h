8f8fbce2908447a9d0d93874ce0d1eb6fc924fa2
LuanFabricio <luanlimaleite@hotmail.com>
2024-01-07 01:37:33 -0300

    fix: Fixing normalizing problem

--- utils.h
+++ utils.h
@@ -2,6 +2,7 @@
 #define __UTILS_H
 
 #include "raylib.h"
+#include "raymath.h"
 
 #define GAME_MAX_X 20
 #define GAME_MIN_X -20
@@ -17,8 +18,8 @@ Vector2 project_game_to_screen(Vector2 point);
 Vector2 project_game_to_screen(Vector2 point)
 {
 	Vector2 p = CLITERAL(Vector2){
-		.x = (point.x/(GAME_MAX_X - GAME_MIN_X)) * GetScreenWidth(),
-		.y = (point.y/(GAME_MAX_Y - GAME_MIN_Y)) * GetScreenHeight(),
+		.x = Normalize(point.x, GAME_MIN_Y, GAME_MAX_X) * GetScreenWidth(),
+		.y = Normalize(point.y, GAME_MIN_Y, GAME_MAX_Y) * GetScreenHeight(),
 	};
 
 	return p;
