a979125840e853976a00b68a234ab11d588331d4
LuanFabricio <luanlimaleite@hotmail.com>
2024-01-09 14:53:06 -0300

    fix: Updating build script

--- build.sh
+++ build.sh
@@ -4,19 +4,19 @@ mkdir -p build
 mkdir -p build/objs
 
 CC=gcc
-CFLAGS="-Wall -Wextra"
+CFLAGS="-Wall -Wextra -g"
 CLINKS="-lm -lraylib"
 
 set -xe
 
 if [[ $1 == "release" ]]; then
-	$CC $CFLAGS -c -o build/objs/player.o src/player.c $CLINKS
+	$CC $CFLAGS -c -o build/objs/game.o src/player.c  $CLINKS
 	$CC $CFLAGS -c -o build/objs/fuel.o src/fuel.c $CLINKS
 
 	OBJS="build/objs/player.o build/objs/fuel.o"
 	$CC $CFLAGS -o build/main main.c $OBJS $CLINKS
 else
-	$CC $CFLAGS -fPIC -shared -o build/objs/player.so src/player.c $CLINKS &
-	$CC $CFLAGS -fPIC -shared -o build/objs/fuel.so src/fuel.c $CLINKS &
+	$CC -g -fPIC -shared -o build/objs/player.so src/player.c $CLINKS -D DEBUG &
+	$CC -g -fPIC -shared -o build/objs/fuel.so src/fuel.c $CLINKS -D DEBUG &
 	$CC $CFLAGS -o build/main main.c $CLINKS -D DEBUG
 fi
