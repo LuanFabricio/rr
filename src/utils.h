25449309e195d20c3766dba2e93f27b6c72ac601
LuanFabricio <luanlimaleite@hotmail.com>
2024-01-09 14:49:02 -0300

    fix: Moving functions to static

--- src/utils.h
+++ src/utils.h
@@ -5,21 +5,23 @@
 #include "raylib.h"
 #include "raymath.h"
 
+#define RR_UTILS static
+
 #define GAME_WIDTH 1024
 #define GAME_HEIGHT 720
+
+#define UNUSED_VAR(x) (void)(x)
 
 #define CREATE_FUNCTION_PTR(signature, name, args, return)\
 	typedef return (*name##_t)args;\
 	signature name##_t name =  NULL
 
-Vector2 screen_center_point();
-Vector2 random_game_point();
-float random_float();
-
-#endif // __UTILS_H
 #ifdef UTILS_IMPLEMENTATION
+#ifndef __UTILS_IMPLEMENTATION
+#define __UTILS_IMPLEMENTATION
 
-Vector2 screen_center_point()
+// TODO: Move functions to specific files
+RR_UTILS Vector2 screen_center_point()
 {
 	return CLITERAL(Vector2) {
 		.x = GAME_WIDTH / 2.f,
@@ -27,17 +29,20 @@ Vector2 screen_center_point()
 	};
 }
 
-Vector2 random_game_point()
+RR_UTILS Vector2 random_game_point()
 {
 	return CLITERAL(Vector2) {
-		.x = rand() % GAME_WIDTH,
-		.y = rand() % GAME_HEIGHT,
+		.x = (float)(rand() % GAME_WIDTH),
+		.y = (float)(rand() % GAME_HEIGHT),
 	};
 }
 
-float random_float()
+RR_UTILS float random_float()
 {
 	return rand() / (double)RAND_MAX;
 }
 
 #endif // UTILS_IMPLEMENTATION
+#endif // __UTILS_IMPLEMENTATION
+
+#endif // __UTILS_H
