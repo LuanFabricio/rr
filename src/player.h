54be95397b1f2a27ce95832e7d5a09b59ac6b2c3
LuanFabricio <luanlimaleite@hotmail.com>
2024-01-09 11:40:05 -0300

    fix: Moving files to src folder

---
+++ src/player.h
@@ -0,0 +1,55 @@
+#ifndef __PLAYER_H
+#define __PLAYER_H
+
+#include <stdbool.h>
+#include "raylib.h"
+
+// #include "utils.h"
+
+typedef struct {
+	Vector2 pos, size;
+	float fuel;
+} Ship;
+
+
+#endif // __PLAYER_H
+
+#ifndef PLAYER_IMPLEMENTATION
+#define PLAYER_IMPLEMENTATION
+
+#define __HOTRELOAD_IMPLEMENTATION
+#include "../hotreload.h"
+#include "utils.h"
+
+#define RR_PLAYER static
+RR_PLAYER void* player_shared_ptr = NULL;
+
+CREATE_FUNCTION_PTR(player_start, (Ship*), void);
+
+CREATE_FUNCTION_PTR(player_draw_game, (Ship), void);
+
+CREATE_FUNCTION_PTR(player_update, (Ship*), void);
+
+CREATE_FUNCTION_PTR(player_draw_ui, (Ship), void);
+
+RR_PLAYER void reset_player_function()
+{
+
+	const char* file_path =  "build/objs/player.so";
+
+	player_shared_ptr = hr_reset_file(file_path, player_shared_ptr);
+
+	player_start = (player_start_t)hr_reset_function(player_shared_ptr, "player_start");
+	player_draw_game = (player_draw_game_t)hr_reset_function(player_shared_ptr, "player_draw_game");
+	player_update = (player_update_t)hr_reset_function(player_shared_ptr, "player_update");
+	player_draw_ui = (player_draw_ui_t)hr_reset_function(player_shared_ptr, "player_draw_ui");
+}
+
+#else
+
+void player_start(Ship *player);
+void player_update(Ship* player);
+void player_draw_game(const Ship player);
+void player_draw_ui(const Ship player);
+
+#endif // PLAYER_IMPLEMENTATION
---
+++ src/utils.h
@@ -0,0 +1,27 @@
+#ifndef __UTILS_H
+#define __UTILS_H
+
+#include "raylib.h"
+#include "raymath.h"
+
+#define GAME_WIDTH 1024
+#define GAME_HEIGHT 720
+
+Vector2 screen_center_point();
+
+#define CREATE_FUNCTION_PTR(name, args, return)\
+	typedef return (*name##_t)args;\
+	RR_PLAYER name##_t name =  NULL
+
+#endif // __UTILS_H
+#ifdef UTILS_IMPLEMENTATION
+
+Vector2 screen_center_point()
+{
+	return CLITERAL(Vector2) {
+		.x = GAME_WIDTH / 2.f,
+		.y = GAME_HEIGHT / 2.f,
+	};
+}
+
+#endif // UTILS_IMPLEMENTATION
