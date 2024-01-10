7d6a52cbc059ff893c3645fa31202b0c8f7ed46d
LuanFabricio <luanlimaleite@hotmail.com>
2024-01-10 13:17:44 -0300

    fix: Moving constants to src/constants.h

---
+++ src/constants.h
@@ -0,0 +1,14 @@
+#ifndef __CONSTANTS_H
+#define __CONSTANTS_H
+
+#include "raylib.h"
+
+#define GAME_WIDTH 1024
+#define GAME_HEIGHT 720
+
+static const Vector2 fuel_size = {
+	.x = 28.f,
+	.y = 42.f,
+};
+
+#endif // __CONSTANTS_H
