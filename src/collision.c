712fa778eda3e0474f415de66b41539ccbd62483
LuanFabricio <luanlimaleite@hotmail.com>
2024-01-13 22:49:45 -0300

    feat: Implementing player vs enemy collision check and updating player vs fuel function name

--- src/collision.c
+++ src/collision.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include "raylib.h"
 
 #include "types.h"
 #include "constants.h"
 
-void collision_check_player(Ship *player, Fuel_Container *container, const Fuel_Functions *fuel_func)
+void collision_check_player_fuel(Ship *player, Fuel_Container *container, const Fuel_Functions *fuel_func)
 {
 	const Rectangle player_rec = {
 		.x = player->pos.x,
@@ -24,6 +25,30 @@ void collision_check_player(Ship *player, Fuel_Container *container, const Fuel_
 
 		if (CheckCollisionRecs(player_rec, other_rec)) {
 			fuel_func->destroy(container, i);
+		}
+	}
+}
+
+void collision_check_player_enemies(Ship *player, Enemies *enemies, const Enemy_Functions *enemy_func)
+{
+	const Rectangle player_rec = {
+		.x = player->pos.x,
+		.y = player->pos.y,
+		.width = player->size.x,
+		.height = player->size.y,
+	};
+
+	Rectangle other_rec = {0};
+
+	for (size_t i = 0; i < enemies->size; i++) {
+		Ship enemy = enemies->data[i];
+		other_rec.x = enemy.pos.x;
+		other_rec.y = enemy.pos.y;
+		other_rec.width = enemy.size.x;
+		other_rec.height = enemy.size.y;
+
+		if (CheckCollisionRecs(player_rec, other_rec)) {
+			player->alive = false;
 		}
 	}
 }
